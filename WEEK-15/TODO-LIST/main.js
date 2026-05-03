5b4b0d90beb4b994c714224d6b7b8982d7d3a27b
Patrick Walshak <patrickwalshak1@gmail.com>
2026-05-03 20:07:06 +0100

    Todo-List

---
+++ WEEK-15/TODO-LIST/main.js
@@ -0,0 +1,32 @@
+        // let taskInput = document.getElementById('taskInput');
+        // let addBtn = document.getElementById('addBtn');
+        // let todoList = document.getElementById('todoList')
+
+        // button.addEventListener("click", function(e) {
+            
+        //     // let task = input.value;
+
+        //     // let li = document.createElement('li');
+
+        //     // li.textContent = task;
+
+        //     // list.appendChild(li)
+
+        //     // input.value='';
+        // }); -->
+// <!-- get ElementByClassName return the list of item  -->
+// <!-- get ElementById return 1 id per element  -->
+
+
+
+// <!-- document.querySelector("div[class ^= "task"]") -->
+
+// <!-- document.querySelectorAll("div[id^='g']") -->
+// <!-- ^ symbol ===> help to select element vlue that start with  -->
+
+// <!-- document.querySelectorAll("div[id*='g']") -->
+//  <!-- * symbol ===>  return the element selected anywhere as long as the element  -->
+
+//  <!--$ symbol ==> tryogn to say that the id that end with the spicific word selected -->
+
+//  <!-- how tot effectively use ur dev too -->
