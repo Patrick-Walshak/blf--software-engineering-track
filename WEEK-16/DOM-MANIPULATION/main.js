ccd726e0f4f67ca808dd299f75b690832a1ac340
Patrick Walshak <patrickwalshak1@gmail.com>
2026-05-04 18:01:19 +0100

    initial commit

--- WEEK-16/DOM-MANIPULATION/main.js
+++
@@ -1,9 +0,0 @@
-const greet = document.querySelector("#greeting")
-const p = document.querySelector("#para")
-
-// console.log(p)
-greet.addEventListener('input', function(e){
-    console.log(e.target.value)
-
-    p.innerHTML = e.target.value
-})
