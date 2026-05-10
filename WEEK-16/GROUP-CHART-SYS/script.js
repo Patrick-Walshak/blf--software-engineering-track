01a1a66f46eab8633d63ce93e5aa72360764354d
Patrick Walshak <patrickwalshak1@gmail.com>
2026-05-10 22:40:34 +0100

    initial commit

--- WEEK-16/GROUP-CHART-SYS/script.js
+++ WEEK-16/GROUP-CHART-SYS/script.js
@@ -3,21 +3,31 @@ const messageInput = document.getElementById("messageInput")
 const sendBtn = document.getElementById("sendBtn")
 
 let messages = JSON.parse(localStorage.getItem("messages")) || []//It tries to get saved messages from the browser, converts them back into an array, and if nothing is found, it just starts with an empty array
-sendBtn.addEventListener("click", (e) =>{
-    e.preventDefault()
 
-    let message = messageInput.value//It takes what the user typed in the input box and stores it in a variable called message
-    messages.push(message)
-    
-    chatBox.innerHTML += ` <div class="flex justify-end">
+function loadMessage(){
+  console.log(messages)
+
+    messages.forEach((message) => {
+      chatBox.innerHTML += ` <div class="flex justify-end">
     <div class="bg-[#8EB69B] text-[#051F20] px-5 py-4 rounded-[25px]">
       ${message}
     </div>
   </div>`
+    });
+} 
+loadMessage()
 
-   localStorage.setItem("messages", JSON.stringify(messages))//It saves your messages into the browser storage.
+sendBtn.addEventListener("click", (e) =>{
+  e.preventDefault()
 
+  
+  let message = messageInput.value//It takes what the user typed in the input box and stores it in a variable called message
+  messages.push(message)
     
+  localStorage.setItem("messages", JSON.stringify(messages))//It saves your messages into the browser storage.
+
+   
+loadMessage()
 messageInput.value = ""
 
 })
