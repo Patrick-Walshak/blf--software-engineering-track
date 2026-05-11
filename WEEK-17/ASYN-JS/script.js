50ec3bc4f9f40f60e354d17db0f69f28a3c210d2
Patrick Walshak <patrickwalshak1@gmail.com>
2026-05-11 13:26:46 +0100

    initial commit

--- WEEK-17/ASYN-JS/script.js
+++ WEEK-17/ASYN-JS/script.js
@@ -1,12 +1,35 @@
-let run = 10
+// let run = 10
 
-let inter = setInterval(() => {
-    console.log("Hello")
-    run -= 1
+// let inter = setInterval(() => {
+//     console.log("Hello")
+//     run -= 1
 
-    if (run < 1){
-        clearInterval(inter)
-    }
+//     if (run < 1){
+//         clearInterval(inter)
+//     }
 
-    console.log(run)
-}, 2000)
+//     console.log(run)
+// }, 2000)
+
+//WEB-API ==> help us to communitcate with aplication that are not browser
+
+
+//PROMISE
+//pending, fulfill, reject
+// let sentEmail = new Promise((resolve, reject) => {
+//     let isSent = "failed"
+
+//     if(isSent == "Sent"){
+//         resolve({success:true, status: isSent, message:"Email sent successfully!"})
+//     }
+    
+//     if(isSent == "Pending"){
+//         resolve({success:true, status: isSent, message:"Email sent successfully!"})
+//     }else{
+//         reject("Email not send!")
+//     }
+// })
+// sentEmail.then((data) => {
+//     console.log(DataTransfer.message)
+// }).catch((erro) =>())
+// console.log(sentEmail)
