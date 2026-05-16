ddd0315a7a95a6447fe5820f2ba444b90de42b34
Patrick Walshak <patrickwalshak1@gmail.com>
2026-05-16 17:46:20 +0100

    initial commit

--- WEEK-17/PROMISE/main.js
+++ WEEK-17/PROMISE/main.js
@@ -43,18 +43,41 @@
 //     console.log(data)
 // })
 
-let checkAge = 18;
-
-let age = new Promise((resolve, reject) => {
-    if(checkAge > 18){
+function checkAge(age) {
+   return new Promise((resolve, reject) => {
+    if(age >= 18){
         resolve("Access Granted")
     }else{
         reject("Access Denied")
     }
 })
+}
 
-age
+
+checkAge(18)
 .then((data) =>{
+    console.log(data)
+})
+
+.catch((data) => {
+    console.log(data)
+})
+
+function orderFood(foodName) {
+   return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(orderFood == "Burger"){
+                resolve("Burder Delivered!")
+            }else{
+                reject("Food not available!")
+            }
+        }, 2000);
+    })
+}
+
+orderFood("Burger")
+
+.then((data) => {
     console.log(data)
 })
 
