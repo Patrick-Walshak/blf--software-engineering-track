549e60f3c20f51ccd588e2367df75c9e6be2d89e
Patrick Walshak <patrickwalshak1@gmail.com>
2026-05-17 15:35:01 +0100

    initial commit

--- WEEK-17/weather-app/main.js
+++ WEEK-17/weather-app/main.js
@@ -4,13 +4,33 @@ const temperature = document.getElementById("temperature")
 const weatherCondition = document.getElementById("weatherCondition")
 const cityName = document.getElementById("cityName")
 
-searchBtn.addEventListener("click", () =>{
-    let city = cityInput.value 
-    console.log(city)
+let apiKey = getApiKey()
 
-    fetch(fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=0ae80774f1c51d4413571dc55be650c2`))
+searchBtn.addEventListener("click", () => {
+
+    let city = cityInput.value
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+
+    .then((response) => {
+        return response.json()
+    })
+
     .then((data) => {
-    console.log(data)
-})
+
+        console.log(data)
+
+        cityName.innerText = data.name
+
+        temperature.innerText = `${Math.round(data.main.temp)}°C`
+
+        weatherCondition.innerText = data.weather[0].main
+
+    })
+
+    .catch((error) => {
+        console.log(error)
+    })
+
 })
 
