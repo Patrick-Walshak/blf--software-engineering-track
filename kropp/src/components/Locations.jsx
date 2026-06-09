37e82dca0fb08f555836233e92703a61a8e406e9
Patrick Walshak <patrickwalshak1@gmail.com>
2026-06-09 17:47:47 +0100

    initial commit

--- kropp/src/components/Locations.jsx
+++ kropp/src/components/Locations.jsx
@@ -3,13 +3,19 @@ import React from 'react'
 function Locations() {
   return (
     <div>
-      <div>
+      <div className="flex">
         <div>
-          <img src="public/images/div#qodef-map-id--347.png" alt="" />
+          <img src="images/location.png" alt="" />
         </div>
 
-        <div>
-
+        <div className="h-[600px] w-[980px] bg-black">
+          <div className="flex justify-center text-center">
+            <div className="text-white mt-44">
+              <h1 className="text-5xl font-bold">FINE US NEAR YOU</h1>
+              <p className="mt-5">Nec suas signiferumque id. An eum labore commodo principes.<br></br> Usu diam noluisse cu, nam adipisci</p>
+              <img className="flex justify-center mx-auto mt-5" src="images/a.qodef-shortcode.png" alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
